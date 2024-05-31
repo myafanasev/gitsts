46a7d6efdb54399b9b2ceca6729d190c8e4e5417
Afanasev Mikhail <miafanasev@gmail.com>
2024-05-31 14:52:23 +0700

    Добавлены пара строк 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("111 1111 22");
+        System.out.println("333");
     }
 }
